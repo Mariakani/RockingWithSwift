8a3771de94f71ec95af82c3c3cd26c15c8e3c750
Mariak Achuoth <achuothmariak@gmail.com>
2019-03-25 16:58:17 -0400

    combine all swift projects into one folder repo

---
+++ Notification Center Projects/Notification Center Projects/HomepageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notification Center Projects
+//
+//  Created by Mariak Achuoth on 2019-03-09.
+//  Copyright © 2019 Mariak Achuoth. All rights reserved.
+//
+
+import UIKit
+
+class HomepageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ Notification Center Projects/Notification Center Projects/SideSelectionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SideSelectionViewController.swift
+//  Notification Center Projects
+//
+//  Created by Mariak Achuoth on 2019-03-09.
+//  Copyright © 2019 Mariak Achuoth. All rights reserved.
+//
+
+import UIKit
+
+class SideSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
